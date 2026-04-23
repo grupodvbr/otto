589974630e1e9a3e929156e24ef10b17e3c78dc0
grupodvbr <grupodvbr@gmail.com>
2026-04-23 14:41:05 -0300

    Update admin-agente.js

--- api/admin-agente.js
+++ api/admin-agente.js
@@ -956,7 +956,7 @@ function toBR(dataISO){
 
 
 
-const API_CUPONS = "https://between-wiley-portrait-essential.trycloudflare.com"
+const API_CUPONS = "http://177.42.253.186:3000"
 
 if(tipoConsulta === "vendas" || tipoConsulta === "relatorio"){
   console.log("📅 DATA FINAL USADA:", dataFiltro)
@@ -2660,7 +2660,7 @@ let dataMes = null
 
 try {
 
-  const API_CUPONS = "https://between-wiley-portrait-essential.trycloudflare.com"
+  const API_CUPONS = "http://177.42.253.186:3000/"
 
   const hoje = new Date(
     new Date().toLocaleString("en-US",{ timeZone:"America/Bahia" })
