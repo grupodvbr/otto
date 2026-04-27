ebccb7065fc5b867602746c6dcb4e0f30045d73f
grupodvbr <grupodvbr@gmail.com>
2026-04-27 08:24:01 -0300

    Update admin-agente.js

--- api/admin-agente.js
+++ api/admin-agente.js
@@ -1059,7 +1059,7 @@ let dadosExternos = {}
 
 try{
 
-  const API = "https://closes-served-targets-david.trycloudflare.com"
+  const API = "https://marked-resolved-tropical-posting.trycloudflare.com"
 
   const [dia, mes, analitico] = await Promise.all([
     fetch(`${API}/resumo-dia?data=${dataFiltro}`).then(r=>r.json()),
@@ -1214,7 +1214,7 @@ function toBR(dataISO){
 
 
 
-const API_CUPONS = "https://closes-served-targets-david.trycloudflare.com"
+const API_CUPONS = "https://marked-resolved-tropical-posting.trycloudflare.com"
 
 if(tipoConsulta === "vendas" || tipoConsulta === "relatorio"){
   console.log("📅 DATA FINAL USADA:", dataFiltro)
@@ -2183,7 +2183,7 @@ let dataMes = null
 
 try {
 
-  const API_CUPONS = "https://closes-served-targets-david.trycloudflare.com"
+  const API_CUPONS = "https://marked-resolved-tropical-posting.trycloudflare.com"
 
   const hoje = new Date(
     new Date().toLocaleString("en-US",{ timeZone:"America/Bahia" })
