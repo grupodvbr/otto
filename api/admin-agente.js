5a05a52c8850f9ece684df73b55b1ec36274b5aa
grupodvbr <grupodvbr@gmail.com>
2026-04-27 19:24:10 -0300

    Update admin-agente.js

--- api/admin-agente.js
+++ api/admin-agente.js
@@ -1058,7 +1058,7 @@ let dadosExternos = {}
 
 try{
 
-  const API = "https://acne-kilometers-myth-veteran.trycloudflare.com"
+  const API = "https://democracy-thesaurus-forms-roland.trycloudflare.com"
 
   const [dia, mes, analitico] = await Promise.all([
     fetch(`${API}/resumo-dia?data=${dataFiltro}`).then(r=>r.json()),
@@ -1213,7 +1213,7 @@ function toBR(dataISO){
 
 
 
-const API_CUPONS = "https://acne-kilometers-myth-veteran.trycloudflare.com"
+const API_CUPONS = "https://democracy-thesaurus-forms-roland.trycloudflare.com"
 
 if(tipoConsulta === "vendas" || tipoConsulta === "relatorio"){
   console.log("📅 DATA FINAL USADA:", dataFiltro)
@@ -2201,7 +2201,7 @@ let dataMes = null
 
 try {
 
-  const API_CUPONS = "https://acne-kilometers-myth-veteran.trycloudflare.com"
+  const API_CUPONS = "https://democracy-thesaurus-forms-roland.trycloudflare.com"
 
   const hoje = new Date(
     new Date().toLocaleString("en-US",{ timeZone:"America/Bahia" })
