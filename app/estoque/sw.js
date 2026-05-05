19247286a1e5da126266c8e79de4bac842c0f891
grupodvbr <grupodvbr@gmail.com>
2026-05-05 18:06:28 -0300

    Update sw.js

--- app/estoque/sw.js
+++ app/estoque/sw.js
@@ -1,19 +1,89 @@
-const CACHE_NAME = "estoque-pwa-v1";
+const CACHE_NAME = "estoque-pwa-v3";
 
 const FILES = [
   "./",
-  "./contagem_do_estoque.html",
-  "./manifest.json"
+  "./contagem_de_estoque.html",
+  "./manifest.json",
+  "./icon-192-2.png",
+  "./icon-512-2.png"
 ];
 
+/* INSTALL */
+
 self.addEventListener("install", event => {
+
+  self.skipWaiting();
+
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+
+    caches
+    .open(CACHE_NAME)
+    .then(cache => cache.addAll(FILES))
+
   );
+
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+/* ACTIVATE */
+
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+
+    caches.keys().then(keys => {
+
+      return Promise.all(
+
+        keys.map(key => {
+
+          if(key !== CACHE_NAME){
+            return caches.delete(key);
+          }
+
+        })
+
+      );
+
+    })
+
   );
+
+  self.clients.claim();
+
+});
+
+/* FETCH */
+
+self.addEventListener("fetch", event => {
+
+  if(event.request.method !== "GET") return;
+
+  event.respondWith(
+
+    caches.match(event.request)
+    .then(response => {
+
+      return response || fetch(event.request)
+      .then(networkResponse => {
+
+        const cloned = networkResponse.clone();
+
+        caches.open(CACHE_NAME)
+        .then(cache => {
+          cache.put(event.request, cloned);
+        });
+
+        return networkResponse;
+
+      });
+
+    })
+    .catch(() => {
+
+      return caches.match("./contagem_de_estoque.html");
+
+    })
+
+  );
+
 });
